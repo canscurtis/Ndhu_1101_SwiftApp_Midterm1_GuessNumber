42f1d71a2a112b56bcb196a2e3cd0799e8347fe4
Curtis Lee <410721310@ndhu.edu.tw>
2021-11-15 12:45:33 +0800

    Final Version

--- swiftMidterm1/ViewController.swift
+++ swiftMidterm1/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var numGue = 0
+    var numAns = 0
+    var nPlayCounts = 0
+    
+    
+    @IBOutlet weak var inputNumber: UITextField!
+    @IBOutlet weak var outputResult: UILabel!
+    @IBOutlet weak var btnRenew: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        numAns = Int.random(in: 1...10)
+        outputResult.text = "Happy\nGuessing"
+        btnRenew.isHidden = true
     }
 
-
+    @IBAction func guessFunc(_ sender: UIButton){
+        nPlayCounts = nPlayCounts+1
+        numGue = Int(inputNumber.text!)!
+        if(nPlayCounts>=5){
+            outputResult.text = "Fail the Game ..."
+        }
+        else if (numGue > numAns) {
+            outputResult.text = "Less than " + String(numGue)
+        }
+        else if (numGue < numAns) {
+            outputResult.text = "Greater than " + String(numGue)
+        }
+        else{
+            outputResult.text = "Correct !!"
+            btnRenew.isHidden = false
+        }
+    }
+    
+    @IBAction func renewFunc(_ sender: UIButton){
+        nPlayCounts = 0
+        numAns = Int.random(in: 1...10)
+        outputResult.text = "Happy\nGuessing"
+        btnRenew.isHidden = true
+    }
+    
 }
 
